0619456c2c51540c73296b7c9047b950faf066c5
Nikita Gaidakov <ngaidakov@podfather.com>
2022-07-26 11:39:09 -0400

    After `pnpm create vite --template vue-ts`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import './hmr';
-import './main.css';
-import App from './App.svelte'
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = new App({
-  target: document.body
-})
-
-export default app
+createApp(App).mount('#app')
