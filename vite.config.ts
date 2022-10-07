9ce52effe0a7410f28bfb00046045cd4997637ea
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-07 23:17:53 +0100

    Add vite-plugin-checker for live vue-tsc checking (+ bump Vite)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    checker({ vueTsc: true }),
   ],
   resolve: {
     alias: {
