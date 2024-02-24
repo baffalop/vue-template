df4a8b3dbfb69c1603b66723c8cfbac6bc0f4a46
Nikita Gaidakov <ngaidakov@podfather.com>
2024-02-24 00:28:19 +0000

    Switch to ts Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import { Config } from 'tailwindcss'
+
+export default {
   mode: 'jit',
   content: [
     './index.html',
@@ -10,4 +12,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+} satisfies Config
