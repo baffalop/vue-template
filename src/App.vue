7ff731f97dedd541f616a60db3e2e0d6764de5f6
Nikita Gaidakov <ngaidakov@podfather.com>
2024-08-28 21:58:48 +0100

    Switch template/script tag order

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="flex items-center">
     <a
@@ -24,6 +18,10 @@ import HelloWorld from '@components/HelloWorld.vue'
 
   <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup lang="ts">
+import HelloWorld from '@components/HelloWorld.vue'
+</script>
 
 <style scoped>
 .logo {
